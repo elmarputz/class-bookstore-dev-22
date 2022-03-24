e7ee70c08f0b7bffbaab842b823de0991cf54611
Elmar Putz <ep@studiomitte.com>
2022-03-24 16:06:28 +0100

    MockData

--- site/views/welcome.php
+++ site/views/welcome.php
@@ -13,4 +13,13 @@
 
 $book = new \Bookshop\Book(1, 1,"asdlfkjasl","alkdjflasdkj", 10);
 var_dump($book);
+echo $book->getId();
+
+$category = new \Bookshop\Category(1, "Testkategorie");
+var_dump($category);
+
+$user = new \Bookshop\User(1, "fritz", "alksdjfl2323");
+var_dump($user);
+
+
 ?>
